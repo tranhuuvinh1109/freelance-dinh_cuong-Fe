0043ab49d491e3021ba5b30a8708d6aed4399b50
VinhHuuTran <tranhuuvinh1109@gmail.com>
2024-01-28 13:24:28 +0700

    done appendix edit and delete

--- src/page/appendix/components/form.jsx
+++ src/page/appendix/components/form.jsx
@@ -1,112 +1,156 @@
-import { DatePicker, Divider, Input, Modal } from 'antd'
-import React from 'react'
-import { CustomSelect } from '../../../components'
-import moment from 'moment'
+/* eslint-disable react/prop-types */
+import { DatePicker, Divider, Input, Modal } from 'antd';
+import { CustomSelect } from '../../../components';
+import { useState } from 'react';
+import appendixData from '../../../constant/appendixAddress.json';
+import { useMutation } from '@tanstack/react-query';
+import appendixAPI from '../../../api/apeendixAPI';
+import { toast } from 'react-hot-toast';
+import { createPortal } from 'react-dom';
+import { LoadingPage } from '../..';
+import dayjs from 'dayjs';
 
-const Form = ({ title, visible, onOk, onCancel, data, setData }) => {
+const Form = ({ visible, title, onCancel, dataEdit, refetch }) => {
+  const [data, setData] = useState(dataEdit);
+  console.log(data.time_back, data.time_move);
+  const { mutate, isLoading } = useMutation({
+    mutationFn: () => appendixAPI.updateAppendix(data?.id, data),
+    onSuccess: (e) => {
+      toast.success(e.data.message);
+      onCancel();
+      refetch();
+    },
+    onError: (e) => {
+      console.error(e);
+      onCancel();
+    },
+  });
+  const addresss = appendixData.map((item) => {
+    return {
+      label: item.location,
+      value: item.location,
+    };
+  });
+
+  const onChange = (e, name) => {
+    setData({
+      ...data,
+      [name]: e,
+    });
+  };
+
+  const onOk = async () => {
+    await mutate();
+  };
   return (
-    <Modal
-      title={<h2 className='text-xl font-semibold'>{title}</h2>}
-      open={visible}
-      onOk={onOk}
-      onCancel={onCancel}
-      width={800}
-      style={{ top: 60 }}
-    >
-      <Divider orientation="left" >Thông tin vị trí</Divider>
-      <div className="md:flex gap-2 w-full">
-        <CustomSelect
-          className='md:w-[33%]'
-          selectTitle='Đài'
-          value={data.dai}
-          onChange={e => setData(prev => ({ ...prev, dai: e.target.value }))}
-        />
-        <CustomSelect
-          className='md:w-[33%]'
-          selectTitle='Cung đoạn'
-          value={data.cungdoan}
-          onChange={e => setData(prev => ({ ...prev, cungdoan: e.target.value }))}
-        />
-        <CustomSelect
-          className='md:w-[33%]'
-          selectTitle='Hệ thống'
-          value={data.hethong}
-          onChange={e => setData(prev => ({ ...prev, hethong: e.target.value }))}
-        />
-      </div>
-      <Divider orientation="left" >Thông tin cáp</Divider>
-      <div className="flex gap-2">
-        <div className='flex gap-2 items-center w-1/2'>
-          <CustomSelect
-            selectTitle='Cáp QH'
-            className='w-full'
-            placeholder=''
-            value={data.capqh}
-            onChange={e => setData(prev => ({ ...prev, capqh: e.target.value }))}
-          />
+    <>
+      <Modal
+        title={<h2 className="text-xl font-semibold">{title}</h2>}
+        open={visible}
+        onOk={onOk}
+        onCancel={onCancel}
+        width={800}
+        style={{ top: 60 }}
+      >
+        <div className="">
+          <Divider orientation="left">Thông tin vị trí</Divider>
+          <div className="md:flex md:justify-between w-full">
+            <CustomSelect
+              className="md:w-[40%]"
+              selectTitle="Cung đoạn"
+              value={data.address}
+              options={addresss}
+              name="address"
+              onChange={(e) => onChange(e, 'address')}
+            />
+            <div className="md:w-[40%]">
+              <span className="text-sm ml-1">Hệ thống</span>
+              <Input
+                className="mt-2 p-1"
+                value={data.system}
+                name="system"
+                onChange={(e) => onChange(e.target.value, 'system')}
+              />
+            </div>
+          </div>
+          <Divider orientation="left">Thông tin cáp</Divider>
+          <div className="">
+            <div className="">
+              <span className="text-sm ml-1">Cáp QH</span>
+              <Input.TextArea
+                className="w-full"
+                placeholder=""
+                name="cable_qh"
+                value={data.cable_qh}
+                onChange={(e) => onChange(e.target.value, 'cable_qh')}
+              />
+            </div>
+            <div className="mt-2">
+              <span className="text-sm ml-1">Cáp UC</span>
+              <Input.TextArea
+                className="w-full"
+                placeholder=""
+                value={data.cable_uc}
+                name="cable_uc"
+                onChange={(e) => onChange(e.target.value, 'cable_uc')}
+              />
+            </div>
+          </div>
+          <div className="flex gap-2 w-full mr-4 mt-6">
+            <div className="flex flex-col w-1/2 gap-2">
+              <span className="text-sm ml-1">Thời gian chuyển đi</span>
+              <DatePicker
+                className="w-full px-[9px] py-[7px] border rounded-md"
+                showTime={{ format: 'HH:mm' }}
+                value={data?.time_move ? dayjs(data?.time_move, 'HH:mm (DD/MM/YYYY)') : null}
+                defaultValue={dayjs()}
+                format="HH:mm (DD/MM/YYYY)"
+                placeholder="Thời gian chuyển đi"
+                onChange={(date) =>
+                  setData((prev) => ({ ...prev, time_move: date ? date.format('HH:mm (DD/MM/YYYY)') : null }))
+                }
+              />
+            </div>
+            <div className="flex flex-col w-1/2 gap-2">
+              <span className="text-sm ml-1">Thời gian chuyển trả</span>
+              <DatePicker
+                className="w-full px-[9px] py-[7px] border rounded-md"
+                showTime={{ format: 'HH:mm' }}
+                value={data?.time_move ? dayjs(data?.time_back, 'HH:mm (DD/MM/YYYY)') : null}
+                defaultValue={dayjs()}
+                format="HH:mm (DD/MM/YYYY)"
+                placeholder="Thời gian chuyển đi"
+                onChange={(date) =>
+                  setData((prev) => ({ ...prev, time_back: date ? date.format('HH:mm (DD/MM/YYYY)') : null }))
+                }
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 mt-4">
+            <span className="text-sm ml-1">Lý do chuyển</span>
+            <Input.TextArea
+              className="w-full"
+              rows={3}
+              value={data.reason}
+              name="reason"
+              onChange={(e) => onChange(e.target.value, 'reason')}
+            />
+          </div>
+          <div className="flex flex-col gap-2 my-4">
+            <span className="text-sm ml-1">Ghi chú</span>
+            <Input.TextArea
+              className="w-full"
+              rows={3}
+              value={data.note}
+              name="note"
+              onChange={(e) => onChange(e.target.value, 'note')}
+            />
+          </div>
         </div>
-        <div className='flex gap-2 items-center w-1/2'>
-          <CustomSelect
-            selectTitle='Cáp ƯC'
-            className='w-full'
-            placeholder=''
-            value={data.capuc}
-            onChange={e => setData(prev => ({ ...prev, capuc: e.target.value }))}
-          />
-        </div>
-      </div>
-      <div className="flex gap-2 w-full mr-4 mt-6">
-        <div className="flex flex-col w-1/2 gap-2">
-          <span className='ml-1'>Thời gian chuyển đi</span>
-          <DatePicker
-            className=" w-full px-[9px] py-[7px] border rounded-md"
-            showTime={{ format: 'HH:mm' }}
-            format="HH:mm (DD/MM/YYYY)"
-            placeholder="Thời gian chuyển đi"
-            value={
-              data && data.tgchuyendi
-                ? moment(data.tgchuyendi, 'HH:mm (DD/MM/YYYY)')
-                : null
-            }
-            onChange={date => setData(prev => ({ ...prev, tgchuyendi: date ? date.format('HH:mm (DD/MM/YYYY)') : null }))}
-          />
-        </div>
-        <div className="flex flex-col w-1/2 gap-2">
-          <span className='ml-1'>Thời gian chuyển trả</span>
-          <DatePicker
-            className=" w-full px-[9px] py-[7px] border rounded-md"
-            showTime={{ format: 'HH:mm' }}
-            format="HH:mm (DD/MM/YYYY)"
-            placeholder="Thời gian chuyển trả"
-            value={
-              data && data.tgchuyentra
-                ? moment(data.tgchuyentra, 'HH:mm (DD/MM/YYYY)')
-                : null
-            }
-            onChange={date => setData(prev => ({ ...prev, tgchuyentra: date ? date.format('HH:mm (DD/MM/YYYY)') : null }))}
-          />
-        </div>
-      </div>
-      <div className="flex flex-col gap-2 mt-4">
-        <span className='ml-1'>Lý do chuyển</span>
-        <Input.TextArea
-          className='w-full'
-          rows={3}
-          value={data.lydochuyen}
-          onChange={e => setData(prev => ({ ...prev, lydochuyen: e.target.value }))}
-        />
-      </div>
-      <div className="flex flex-col gap-2 my-4">
-        <span className='ml-1'>Ghi chú</span>
-        <Input.TextArea
-          className='w-full'
-          rows={3}
-          value={data.ghichu}
-          onChange={e => setData(prev => ({ ...prev, ghichu: e.target.value }))}
-        />
-      </div>
-    </Modal>
-  )
-}
+      </Modal>
+      {isLoading && createPortal(<LoadingPage />, document.body)}
+    </>
+  );
+};
 
-export default Form
+export default Form;
--- src/page/createAppendix/createAppendix.jsx
+++ src/page/createAppendix/createAppendix.jsx
@@ -2,11 +2,25 @@ import { DatePicker, Divider, Input } from 'antd';
 import { CustomSelect } from '../../components';
 import { initialStateAppendix } from '../../constant/init';
 import { useState } from 'react';
-import dayjs from 'dayjs';
 import appendixData from '../../constant/appendixAddress.json';
+import { useMutation } from '@tanstack/react-query';
+import appendixAPI from '../../api/apeendixAPI';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { IoChevronBackOutline } from 'react-icons/io5';
+import Loading from '../loading/Loading';
 
 const CreateAppendix = () => {
+  const navigate = useNavigate();
   const [data, setData] = useState(initialStateAppendix);
+  const { mutate, isLoading } = useMutation({
+    mutationFn: () => appendixAPI.createAppendix(data),
+    onSuccess: (e) => {
+      toast.success(e.data.message);
+      setData(initialStateAppendix);
+      navigate('/appendix');
+    },
+  });
   const addresss = appendixData.map((item) => {
     return {
       label: item.location,
@@ -15,106 +29,139 @@ const CreateAppendix = () => {
   });
 
   const onChange = (e, name) => {
-    if (name === 'address') {
-      setData((prev) => ({ ...prev, [name]: e }));
+    setData({
+      ...data,
+      [name]: e,
+    });
+  };
+  const onBack = () => navigate(-1);
+  const onSubmit = async () => {
+    if (!data.address) {
+      toast.error('Vui lòng chọn cung đoạn');
+      return;
     }
+    await mutate();
   };
   return (
-    <div className="mt-24">
-      <div className="md:flex md:justify-center mt-24 overflow-hidden">
-        <div className="w-full md:w-3/5 md:border p-6 rounded-md bg-white md:shadow-xl">
-          <Divider orientation="left">Thông tin vị trí</Divider>
-          <div className="md:flex gap-2 w-full">
-            <CustomSelect
-              className="md:w-[33%]"
-              selectTitle="Đài"
-              value={data.address}
-              options={addresss}
-              onChange={(e) => onChange(e, 'address')}
-            />
-            <CustomSelect
-              className="md:w-[33%]"
-              selectTitle="Cung đoạn"
-              value={data.cungdoan}
-              onChange={(e) => setData((prev) => ({ ...prev, cungdoan: e.target.value }))}
-            />
-            <CustomSelect
-              className="md:w-[33%]"
-              selectTitle="Hệ thống"
-              value={data.hethong}
-              onChange={(e) => setData((prev) => ({ ...prev, hethong: e.target.value }))}
-            />
-          </div>
-          <Divider orientation="left">Thông tin cáp</Divider>
-          <div className="flex gap-2">
-            <div className="flex gap-2 items-center w-1/2">
+    <>
+      <div className="mt-24">
+        <div className="md:flex md:justify-center mt-24 overflow-hidden">
+          <div className="w-full md:w-3/5 md:border p-6 rounded-md bg-white md:shadow-xl">
+            <button
+              onClick={onBack}
+              className="flex items-center gap-1 bg-slate-300 px-6 py-2 rounded-md hover:bg-slate-500"
+            >
+              <IoChevronBackOutline />
+              <span className="font-semibold">Back</span>
+            </button>
+            <Divider orientation="left">Thông tin vị trí</Divider>
+            <div className="md:flex md:justify-between w-full">
               <CustomSelect
-                selectTitle="Cáp QH"
+                className="md:w-[40%]"
+                selectTitle="Cung đoạn"
+                value={data.address}
+                options={addresss}
+                name="address"
+                onChange={(e) => onChange(e, 'address')}
+              />
+              <div className="md:w-[40%]">
+                <span className="text-sm ml-1">Hệ thống</span>
+                <Input
+                  className="mt-2 p-1"
+                  value={data.system}
+                  name="system"
+                  onChange={(e) => onChange(e.target.value, 'system')}
+                />
+              </div>
+            </div>
+            <Divider orientation="left">Thông tin cáp</Divider>
+            <div className="">
+              <div className="">
+                <span className="text-sm ml-1">Cáp QH</span>
+                <Input.TextArea
+                  className="w-full"
+                  placeholder=""
+                  name="cable_qh"
+                  value={data.cable_qh}
+                  onChange={(e) => onChange(e.target.value, 'cable_qh')}
+                />
+              </div>
+              <div className="mt-2">
+                <span className="text-sm ml-1">Cáp UC</span>
+                <Input.TextArea
+                  className="w-full"
+                  placeholder=""
+                  value={data.cable_uc}
+                  name="cable_uc"
+                  onChange={(e) => onChange(e.target.value, 'cable_uc')}
+                />
+              </div>
+            </div>
+            <div className="flex gap-2 w-full mr-4 mt-6">
+              <div className="flex flex-col w-1/2 gap-2">
+                <span className="text-sm ml-1">Thời gian chuyển đi</span>
+                <DatePicker
+                  className=" w-full px-[9px] py-[7px] border rounded-md"
+                  showTime={{ format: 'HH:mm' }}
+                  format="HH:mm (DD/MM/YYYY)"
+                  placeholder="Thời gian chuyển đi"
+                  onChange={(date) =>
+                    setData((prev) => ({ ...prev, time_move: date ? date.format('HH:mm (DD/MM/YYYY)') : null }))
+                  }
+                />
+              </div>
+              <div className="flex flex-col w-1/2 gap-2">
+                <span className="text-sm ml-1">Thời gian chuyển trả</span>
+                <DatePicker
+                  className=" w-full px-[9px] py-[7px] border rounded-md"
+                  showTime={{ format: 'HH:mm' }}
+                  format="HH:mm (DD/MM/YYYY)"
+                  placeholder="Thời gian chuyển trả"
+                  onChange={(date) =>
+                    setData((prev) => ({ ...prev, time_back: date ? date.format('HH:mm (DD/MM/YYYY)') : null }))
+                  }
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 mt-4">
+              <span className="text-sm ml-1">Lý do chuyển</span>
+              <Input.TextArea
                 className="w-full"
-                placeholder=""
-                value={data.capqh}
-                onChange={(e) => setData((prev) => ({ ...prev, capqh: e.target.value }))}
+                rows={3}
+                value={data.reason}
+                name="reason"
+                onChange={(e) => onChange(e.target.value, 'reason')}
               />
             </div>
-            <div className="flex gap-2 items-center w-1/2">
-              <CustomSelect
-                selectTitle="Cáp ƯC"
+            <div className="flex flex-col gap-2 my-4">
+              <span className="text-sm ml-1">Ghi chú</span>
+              <Input.TextArea
                 className="w-full"
-                placeholder=""
-                value={data.capuc}
-                onChange={(e) => setData((prev) => ({ ...prev, capuc: e.target.value }))}
+                rows={3}
+                value={data.note}
+                name="note"
+                onChange={(e) => onChange(e.target.value, 'note')}
               />
             </div>
-          </div>
-          <div className="flex gap-2 w-full mr-4 mt-6">
-            <div className="flex flex-col w-1/2 gap-2">
-              <span className="ml-1">Thời gian chuyển đi</span>
-              <DatePicker
-                className=" w-full px-[9px] py-[7px] border rounded-md"
-                showTime={{ format: 'HH:mm' }}
-                format="HH:mm (DD/MM/YYYY)"
-                placeholder="Thời gian chuyển đi"
-                value={dayjs(data.time_move)}
-                onChange={(date) =>
-                  setData((prev) => ({ ...prev, tgchuyendi: date ? date.format('HH:mm (DD/MM/YYYY)') : null }))
-                }
-              />
+            <div className="flex justify-end">
+              <button
+                className="mr-4 bg-orange-400 font-semibold text-white px-14 py-2 rounded-md hover:bg-orange-600"
+                onClick={() => setData(initialStateAppendix)}
+              >
+                Clear
+              </button>
+              <button
+                className="bg-[var(--main-theme-color)] font-semibold text-white px-14 py-2 rounded-md hover:bg-[var(--main-theme-color-hover)]"
+                onClick={onSubmit}
+              >
+                Submit
+              </button>
             </div>
-            <div className="flex flex-col w-1/2 gap-2">
-              <span className="ml-1">Thời gian chuyển trả</span>
-              <DatePicker
-                className=" w-full px-[9px] py-[7px] border rounded-md"
-                showTime={{ format: 'HH:mm' }}
-                format="HH:mm (DD/MM/YYYY)"
-                placeholder="Thời gian chuyển trả"
-                value={dayjs(data.time_back)}
-                onChange={(date) =>
-                  setData((prev) => ({ ...prev, tgchuyentra: date ? date.format('HH:mm (DD/MM/YYYY)') : null }))
-                }
-              />
-            </div>
-          </div>
-          <div className="flex flex-col gap-2 mt-4">
-            <span className="ml-1">Lý do chuyển</span>
-            <Input.TextArea
-              className="w-full"
-              rows={3}
-              value={data.lydochuyen}
-              onChange={(e) => setData((prev) => ({ ...prev, lydochuyen: e.target.value }))}
-            />
-          </div>
-          <div className="flex flex-col gap-2 my-4">
-            <span className="ml-1">Ghi chú</span>
-            <Input.TextArea
-              className="w-full"
-              rows={3}
-              value={data.ghichu}
-              onChange={(e) => setData((prev) => ({ ...prev, ghichu: e.target.value }))}
-            />
           </div>
         </div>
       </div>
-    </div>
+      {isLoading && <Loading />}
+    </>
   );
 };
 
