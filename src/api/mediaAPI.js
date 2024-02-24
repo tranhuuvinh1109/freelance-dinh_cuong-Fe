d781f6dad809277332459660aa814ca0be13d41e
VinhHuuTran <tranhuuvinh1109@gmail.com>
2024-02-24 15:10:42 +0700

    update view table

--- src/api/mediaAPI.js
+++ src/api/mediaAPI.js
@@ -4,6 +4,7 @@ const mediaAPI = {
   downloadFile: () => axiosClient.get('/download'),
   getDataSheet: (file, sheet) => axiosClient.get(`get/${file}/${sheet}`),
   getDataMedia: () => axiosClient.get(`media-formated/`),
+  getAllMedia: () => axiosClient.get(`media/`),
   createMedia: (params) => axiosClient.post(`media/create`, params),
 };
 export default mediaAPI;
--- src/page/viewExcel/viewExcel.jsx
+++ src/page/viewExcel/viewExcel.jsx
@@ -1,69 +1,39 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import mediaAPI from '../../api/mediaAPI';
-import SheetCard from './components/SheetCard/SheetCard';
 import { IoChevronBackOutline } from 'react-icons/io5';
 import { createPortal } from 'react-dom';
 import { LoadingPage } from '..';
+import { Table } from 'antd';
+import { useQuery } from '@tanstack/react-query';
+const { Column, ColumnGroup } = Table;
 
 const ViewExcel = () => {
-  const [dataExcel, setDataExcel] = useState();
-  const [isLoading, setIsLoading] = useState(true);
+  const { data, isLoading } = useQuery({
+    queryKey: ['appendix'],
+    queryFn: () => mediaAPI.getAllMedia(),
+  });
   const convertTable = useMemo(() => {
-    if (dataExcel?.length > 0) {
+    if (data?.data?.length > 0) {
       return (
-        <>
-          <div className="flex font-semibold">
-            <div className="w-[8%] border-y border-x border-zinc-900 px-2 py-1 flex items-center ">Cung đoạn</div>
-            <div className="w-[16%] border-y border-r border-zinc-900 px-2 py-1 flex items-center">Tên TTCQ</div>
-            <div className="w-[8%] border-y border-r border-zinc-900 px-2 py-1 flex items-center">Số điện thoại</div>
-            <div className="w-[42%] border-y border-r border-zinc-900 py-1">
-              <div className="text-center border-b border-zinc-900 px-2 py-1">Thông tin nhà dân / Cơ quan</div>
-              <div className="flex">
-                <div className="w-[32%] border-r border-zinc-900 px-2 py-1 flex items-center">Km đường bộ</div>
-                <div className="w-[24%] border-r border-zinc-900 px-2 py-1 flex items-center">Tên </div>
-                <div className="w-[24%] border-r border-zinc-900 px-2 py-1 flex items-center">Số điện thoại</div>
-                <div className="w-[20%] flex items-center px-2 py-1">Ngày</div>
-              </div>
-            </div>
-            <div className="w-[26%] flex items-center border-y border-r border-zinc-900 px-2 py-1">Ghi chú</div>
-          </div>
-          {dataExcel.map((row, index) => {
-            return <SheetCard data={row} key={index} />;
-          })}
-        </>
+        <div>
+          <Table dataSource={data.data} scroll={{ x: 'max-content' }}>
+            <Column title="Cung đoạn" dataIndex="location" key="location" />
+            <Column title="Tên TTCQ" dataIndex="name_ttcq" key="name_ttcq" />
+            <Column title="Số điện thoại" dataIndex="phone_staff" key="phone_staff" />
+            <ColumnGroup title="Thông tin nhà dân / Cơ quan">
+              <Column title="Km đường bộ" dataIndex="km" key="km" />
+              <Column title="Tên" dataIndex="name" key="name" />
+              <Column title="Số điện thoại" dataIndex="phone" key="phone" />
+              <Column title="Ngày" dataIndex="date" key="date" />
+            </ColumnGroup>
+            <Column title="Ghi chú" dataIndex="note" key="note" />
+          </Table>
+        </div>
       );
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dataExcel]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        const res = await mediaAPI.getDataMedia();
-        console.log(res);
-        if (res.status === 200) {
-          const result = res.data.reduce((acc, curr) => {
-            const location = curr[0];
-            const existingItem = acc.find((item) => item.location === location);
+  }, [data]);
 
-            if (existingItem) {
-              existingItem.items.push(curr);
-            } else {
-              acc.push({ location, items: [curr] });
-            }
-
-            return acc;
-          }, []);
-          setDataExcel(result);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-      setIsLoading(false);
-    };
-    fetchData();
-  }, []);
   return (
     <>
       {isLoading && createPortal(<LoadingPage />, document.body)}
