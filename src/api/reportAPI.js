052524ded4fa995afc5f9e088b73ac5c6fb4218f
VinhHuuTran <tranhuuvinh1109@gmail.com>
2024-02-24 14:48:08 +0700

    fix new issue 24/2

--- src/api/reportAPI.js
+++ src/api/reportAPI.js
@@ -2,6 +2,7 @@ import axiosClient from './axiosClient';
 
 const reportAPI = {
   downloadReport: () => axiosClient.get('report/download'),
+  deleteReport: (id) => axiosClient.delete(`report/delete/${id}`),
   createReport: (params) => axiosClient.post('report/create', params),
   getReportByLocationAndDate: (location, date) => axiosClient.get(`report/${location}/${date}`),
   getAllReport: () => axiosClient.get(`report/get-all`),
